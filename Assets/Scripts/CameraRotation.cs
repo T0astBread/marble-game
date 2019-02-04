6ef43c6325ab0f3dc8c22a134ed88569567279ee
T0astBread <tb@t0ast.cc>
2019-02-04 23:59:19 +0100

    Added camera rotation and according rotation of the marble's movement

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+	private Transform playerTransform;
+	private MovementControls playerControls;
+
+	void Start()
+	{
+		var player = GameObject.Find("Marble");
+		this.playerTransform = player.transform;
+		this.playerControls = player.GetComponent<MovementControls>();
+	}
+
+	void Update()
+	{
+		UpdateCameraRotation();
+		UpdatePlayerForward();
+	}
+
+	private void UpdateCameraRotation()
+	{
+		var angleX = transform.rotation.eulerAngles.x - Input.GetAxis("Mouse Y");
+		var angleY = transform.rotation.eulerAngles.y + Input.GetAxis("Mouse X");
+		transform.rotation = Quaternion.Euler(angleX, angleY, 0);
+	}
+
+	private void UpdatePlayerForward()
+	{
+		this.playerControls.forwardAngle = -Vector3.SignedAngle(Vector3.forward, transform.forward, Vector3.up) - 180;
+	}
+}
--- Assets/Scripts/MovementControls.cs
+++ Assets/Scripts/MovementControls.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class MovementControls : MonoBehaviour
 {
+	public float speed = 1000;
+
+	public float forwardAngle;
+
 	private new Rigidbody rigidbody;
 
 	void Start()
 	{
 		this.rigidbody = GetComponent<Rigidbody>();
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	void Update()
 	{
-		var input = GetInputVector() * 1000;
-		// this.rigidbody.velocity = new Vector3(input.x, this.rigidbody.velocity.y, input.z);
-		// this.rigidbody.rotation *= Quaternion.AngleAxis(input.x, Vector3.forward) * Quaternion.AngleAxis(input.y, Vector3.right);
-		this.rigidbody.AddTorque(input.z, 0, -input.x);
+		var input = GetInputVector();
+		var torque = input.Rotate(this.forwardAngle) * this.speed;
+		this.rigidbody.AddTorque(torque.x, 0, torque.y);
 		this.rigidbody.maxAngularVelocity = 100;
 	}
 
-	private Vector3 GetInputVector()
+	private Vector2 GetInputVector()
 	{
 		var velX = Input.GetAxisRaw("Horizontal");
 		var velY = Input.GetAxisRaw("Vertical");
-		return new Vector3(velX, 0, velY);
+		return new Vector2(-velY, velX);
 	}
 }
