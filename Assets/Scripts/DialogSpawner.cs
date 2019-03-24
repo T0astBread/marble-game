34317f4c0ec82a26fb519d77e2836c46e333cadd
T0astBread <tb@t0ast.cc>
2019-03-24 02:13:54 +0100

    Added ability to interact with sign
    Right now it just displays some log messages

---
+++ Assets/Scripts/DialogSpawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DialogSpawner : MonoBehaviour
+{
+	void StartInteraction(GameObject interactor)
+	{
+		Debug.Log("*Dialog appears, triggered by" + interactor.name + "*");
+	}
+}
---
+++ Assets/Scripts/InteractionControls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionControls : MonoBehaviour
+{
+	private GameObject currentInteractableObject;
+	private JumpControls jumpControls;
+
+	void Start()
+	{
+		this.jumpControls = GetComponent<JumpControls>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && this.currentInteractableObject != null)
+		{
+			Debug.Log(this.currentInteractableObject.name);
+			this.currentInteractableObject.SendMessageUpwards("StartInteraction", gameObject);
+		}
+	}
+
+	void OnInteractiveObjectEnter(GameObject interactiveObject)
+	{
+		this.currentInteractableObject = interactiveObject;
+
+		if (this.jumpControls != null)
+		{
+			this.jumpControls.enabled = false;
+		}
+	}
+
+	void OnInteractiveObjectExit(GameObject interactiveObject)
+	{
+		this.currentInteractableObject = null;
+
+		if (this.jumpControls != null)
+		{
+			this.jumpControls.enabled = true;
+		}
+	}
+}
