3aab235123f705c79f667dce10241a79cdd752d1
T0astBread <tb@t0ast.cc>
2019-04-14 20:11:04 +0200

    Added acceleration script to ramp

---
+++ Assets/Scripts/AccelerateColliding.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerateColliding : MonoBehaviour {
+	public float factor = 10;
+
+	void OnCollisionStay(Collision collision) {
+		var rigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+		if (rigidbody != null) {
+			Debug.Log(rigidbody.velocity * Time.deltaTime * this.factor);
+			rigidbody.AddForce(rigidbody.velocity * Time.deltaTime * this.factor, ForceMode.Acceleration);
+		}
+	}
+}
